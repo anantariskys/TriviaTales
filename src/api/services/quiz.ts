8e5a3b1d99feb522cab5c8c5cbf798d8ac0dbd23
anantariskys <gamersindo555@gmail.com>
2024-12-28 00:17:08 +0700

    Create quiz.ts

---
+++ src/api/services/quiz.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import coreApi from "../coreApi";
+
+const getQuestion = async (categoryId:number,totalQuestion:number) => {
+  try {
+    const response = await axios.get(
+      `https://opentdb.com/api.php?amount=${totalQuestion}&category=${categoryId}&type=multiple`
+    );
+    const data = response.data.results;
+
+    const formattedQuestions = data.map((q: any) => ({
+      question: q.question,
+      correctAnswer: q.correct_answer,
+      allAnswers: [...q.incorrect_answers, q.correct_answer].sort(
+        () => Math.random() - 0.5
+      ), // Gabungkan dan acak
+    }));
+    return formattedQuestions;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getCategory = async () => {
+  try {
+    const response = await axios.get("https://opentdb.com/api_category.php");
+    const data = response.data.trivia_categories;
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+export { getQuestion,getCategory };
