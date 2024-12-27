ae12fc4ed028ee56ef2a49efa832f0bc0c461d21
anantariskys <gamersindo555@gmail.com>
2024-12-28 00:17:30 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import Route from "./routes/Route.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+ 
+    <GoogleOAuthProvider clientId="472902479167-andhl8f553i9e3dqh01isegcjudfsnfh.apps.googleusercontent.com">
+      <Route />
+    </GoogleOAuthProvider>
+);
