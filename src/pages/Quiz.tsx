0187f1e249a1ef2f2fd9d8bdaba2e0080e0e5a5d
anantariskys <gamersindo555@gmail.com>
2024-12-28 15:37:22 +0700

    Update Quiz.tsx

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import  { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 import { getQuestion } from "../api/services/quiz";
