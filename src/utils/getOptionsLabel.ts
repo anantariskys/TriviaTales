8f584521c69bf677eaaeff3cdf77ce63f3aa3c50
anantariskys <gamersindo555@gmail.com>
2024-12-29 14:57:24 +0700

    Create getOptionsLabel.ts

---
+++ src/utils/getOptionsLabel.ts
@@ -0,0 +1,24 @@
+const getOptionsLabel = (index : number) => {
+    switch (index) {
+        case 0:
+        return "A";
+            break;
+        case 1:
+            return "B";
+            
+            break;
+        case 2:
+            return "C";
+            
+            break;
+        case 3:
+            return "D";
+            break;
+        default:
+            break;
+    }
+}
+
+
+
+export default getOptionsLabel
