a01b922fe6fa33a2dcf55a5ff8de2154db6831d1
anantariskys <gamersindo555@gmail.com>
2024-12-29 11:11:44 +0700

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -31,13 +31,13 @@ const Login: React.FC = () => {
     <div className="h-screen flex justify-center relative bg-center bg-cover" style={{backgroundImage: `url(${Bg})`}}>
       <div className="bg-black bg-opacity-35 absolute inset-0 "></div>
       <main className="w-full container relative z-20 hidden lg:flex justify-center flex-col">
-        <div className="text-white space-y-4">
-          <h1 className="text-white font-medium text-4xl md:text-6xl lg:text-8xl">Welcome to <span className="text-primary font-bold ">Trivia Tales</span></h1>
-          <p className="text-lg">"Welcome to TriviaTales — Where Curiosity Meets Challenge! Dive into a world of intriguing questions, exciting challenges, and endless discoveries. Unleash your knowledge, test your limits, and let every answer pave the way to becoming a true trivia champion!"</p>
+        <div className="text-tertiary space-y-4">
+          <h1 className="text-white font-medium text-4xl md:text-6xl lg:text-7xl">Welcome to <span className="text-primary font-bold ">Trivia</span> <span className="text-secondary font-bold">Tales</span></h1>
+          <p className="">"Welcome to TriviaTales — Where Curiosity Meets Challenge! Dive into a world of intriguing questions, exciting challenges, and endless discoveries. Unleash your knowledge, test your limits, and let every answer pave the way to becoming a true trivia champion!"</p>
         </div>
       </main>
       <aside className="max-w-md relative z-20 w-full flex flex-col items-center justify-center bg-white bg-opacity-30 backdrop-blur-sm space-y-2 p-8 gap-4">
-        <div className="w-full space-y-2 text-center text-white ">
+        <div className="w-full space-y-2 text-center text-tertiary ">
           <h1 className="text-6xl font-semibold">Login</h1>
           <p className="text-base">Get started and make learning exciting today!</p>
         </div>
