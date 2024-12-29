8e14d45238cb545f386819205382036064ccc54f
anantariskys <gamersindo555@gmail.com>
2024-12-29 14:57:00 +0700

    Create AnswerIcon.tsx

---
+++ src/components/results/AnswerIcon.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Icon } from '@iconify/react';
+
+interface AnswerIconsProps {
+  isCorrect: boolean;
+}
+
+const AnswerIcons: React.FC<AnswerIconsProps> = ({ isCorrect }) => (
+  <div className="flex items-center justify-center bg-tertiary gap-2 py-1 rounded-md px-3">
+    {isCorrect ? (
+        <>
+      <Icon icon="mdi:check-circle" className="text-green-500" width="24" height="24" />
+      <p>benar</p>
+        </>
+    ) : (
+        <>
+      <Icon icon="mdi:close-circle" className="text-red-500" width="24" height="24" />
+        <p>salah    </p>
+        </>
+    )}
+  </div>
+);
+
+export default AnswerIcons;
