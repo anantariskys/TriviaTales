7ecefc64a7b0ca0c6f10cbe9aa79aaf8dfa18137
anantariskys <gamersindo555@gmail.com>
2024-12-30 11:23:32 +0700

    Update LoadingScreen.tsx
    Style : optimize height container

--- src/components/quiz/LoadingScreen.tsx
+++ src/components/quiz/LoadingScreen.tsx
@@ -1,13 +1,16 @@
+import Modal from "../Modal";
+
 const LoadingScreen = () => {
   return (
     <div
-      className="flex items-center justify-center h-screen"
+      className="flex items-center justify-center min-h-screen"
       style={{
         backgroundColor: "#f4f6ff",
         backgroundImage:
           "url(https://www.transparenttextures.com/patterns/inspiration-geometry.png)",
       }}
     >
+         <Modal />
       <div className="text-center">
         <div className="w-12 h-12 border-8 mx-auto border-primary border-t-transparent rounded-full animate-spin"></div>
         <p className="mt-4 text-primary text-lg  animate-bounce font-semibold">
