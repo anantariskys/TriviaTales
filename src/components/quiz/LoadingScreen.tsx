a617d1fac4eb74ee810cbd2c0510ea8e497e2034
anantariskys <gamersindo555@gmail.com>
2024-12-30 07:58:13 +0700

    feat(component): add LoadingScreen
    for display loading state

---
+++ src/components/quiz/LoadingScreen.tsx
@@ -0,0 +1,21 @@
+const LoadingScreen = () => {
+  return (
+    <div
+      className="flex items-center justify-center h-screen"
+      style={{
+        backgroundColor: "#f4f6ff",
+        backgroundImage:
+          "url(https://www.transparenttextures.com/patterns/inspiration-geometry.png)",
+      }}
+    >
+      <div className="text-center">
+        <div className="w-12 h-12 border-8 mx-auto border-primary border-t-transparent rounded-full animate-spin"></div>
+        <p className="mt-4 text-primary text-lg  animate-bounce font-semibold">
+          Loading questions...
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingScreen;
