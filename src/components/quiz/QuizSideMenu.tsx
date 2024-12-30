de81f1a7c81dd77f83a280841ef04fc16cff658b
anantariskys <gamersindo555@gmail.com>
2024-12-30 11:25:36 +0700

    Update QuizSideMenu.tsx
    Refactor : Split to reusable component

--- src/components/quiz/QuizSideMenu.tsx
+++ src/components/quiz/QuizSideMenu.tsx
@@ -2,22 +2,18 @@ import React from "react";
 import { useQuizTypeStore } from "../../store/useQuizTypeStore";
 import { useQuizStore } from "../../store/useQuizStore";
 import Button from "../Button";
-import formatTime from "../../utils/formatTime";
 import getAnsweredQuestions from "../../utils/getAnsweredQuestion";
 import calculateResult from "../../utils/calculateResult";
 import { useNavigate } from "react-router-dom";
+import QuestionNavigatorItem from "./QuestionNavigatorItem";
+import SideMenuHeading from "./SideMenuHeading";
+import QuestionNavigatorLayout from "./QuestionNavigatorLayout";
+import { useModalStore } from "../../store/useModalStore";
 
 const QuizSideMenu: React.FC = () => {
-  const { selectedCategoryName, resetType } = useQuizTypeStore();
-  const {
-    setResults,
-    answers,
-    questions,
-    currentQuestionIndex,
-    timer,
-    setCurrentQuestionIndex,
-    reset,
-  } = useQuizStore();
+  const { resetType } = useQuizTypeStore();
+  const { setResults, answers, questions, reset } = useQuizStore();
+  const { openModal } = useModalStore();
   const navigate = useNavigate();
 
   const handleFinishQuiz = () => {
@@ -38,38 +34,27 @@ const QuizSideMenu: React.FC = () => {
     reset();
     resetType();
   };
+
+  const handleFinishQuizWithConfirmation = () => {
+    openModal("Are you sure you want to finish the quiz?", handleFinishQuiz);
+  };
+
   return (
     <aside className="w-full md:max-w-xs h-fit bg-primary text-tertiary shadow-lg rounded-lg p-6 space-y-4">
-      <h1 className="text-secondary font-semibold text-2xl">Trivia Tales</h1>
-      <h1>Category : {selectedCategoryName}</h1>
-      <div className="px-4 p-2 bg-tertiary text-primary rounded-md w-fit">
-        Remaining Time : {formatTime(timer)}
-      </div>
-      <div className="grid grid-cols-5 gap-2">
+      <SideMenuHeading />
+      <QuestionNavigatorLayout>
         {questions.length > 0 &&
           Array.from({ length: questions.length }).map((_, index) => (
-            <div
-              onClick={() => setCurrentQuestionIndex(index)}
-              key={index}
-              className={`${
-                currentQuestionIndex === index
-                  ? "bg-slate-400 "
-                  : answers[index]
-                  ? "bg-secondary"
-                  : "bg-tertiary text-primary"
-              } aspect-square rounded-lg border border-tertiary hover:bg-opacity-80 flex items-center justify-center cursor-pointer transition`}
-            >
-              {index + 1}
-            </div>
+            <QuestionNavigatorItem index={index} key={index} />
           ))}
-      </div>
+      </QuestionNavigatorLayout>
       <Button
-        onClick={handleFinishQuiz}
+        onClick={handleFinishQuizWithConfirmation} 
         variant="secondary"
         width="w-full"
         className="mt-4"
       >
-        Selesai
+        Finish
       </Button>
     </aside>
   );
